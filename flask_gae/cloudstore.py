e13db15c540f117eb1d2660bf00f2cef585789e9
Chris Targett <chris@xlevus.net>
2014-08-05 16:53:01 +0100

    Added some speed improvements

--- flask_gae/cloudstore.py
+++ flask_gae/cloudstore.py
@@ -8,10 +8,35 @@ from google.appengine.ext import blobstore
 logger = logging.getLogger(__name__)
 
 
-def send_gcs_file(filename, bucket=None, mimetype=None, add_etags=True,
+class LazyStat(object):
+    """Class to lazily call stat()"""
+    # TODO: Make this Async
+    # Dependent on
+    # https://code.google.com/p/appengine-gcs-client/issues/detail?id=13
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = None
+
+    def __getattr__(self, attr):
+        if self.data is None:
+            self.data = gcs.stat(self.filename)
+        return getattr(self.data, attr)
+
+
+def send_gcs_file(filename, bucket=None, mimetype=None,
+                  add_etags=True, etags=None,
+                  add_last_modified=True, last_modified=None,
                   as_attachment=False, attachment_filename=None):
     """
     Serve a file in Google Cloud Storage (gcs) to the client.
+
+    ..note:: When `add_etags`, `add_last_modified` or no `mimetype` is
+      provided, two extra RPC calls will be made to retrieve data from
+      Cloud Storage.
+      If peformance, is a priority, it is advised to provide values for these
+      parameters or cache the response with memcache. **But** this will return
+      500 responses if the file does not exist in GCS.
+
 
     :param filename: The filepath to serve from gcs.
 
@@ -23,6 +48,11 @@ def send_gcs_file(filename, bucket=None, mimetype=None, add_etags=True,
         unknown files is `application/octet-stream`.
 
     :param add_etags: If `True`, etags as provided by gcs will be added.
+    :param etags: Override any etags from GCS.
+
+    :param add_last_modified: If `True` the last-modified header will be added
+        using the value from GCS.
+    :param last_modified: Override the last-modified value from GCS.
 
     :param as_attachment: set to `True` if you want to send this file with a
         ``Content-Disposition: attachment`` header.
@@ -35,34 +65,34 @@ def send_gcs_file(filename, bucket=None, mimetype=None, add_etags=True,
 
         bucket = bucket or app_identity.get_default_gcs_bucket_name()
         gcs_filename = '/{}/{}'.format(bucket, filename)
-        blobkey = blobstore.create_gs_key('/gs' + gcs_filename)
+        blobkey = blobstore.create_gs_key_async('/gs' + gcs_filename)
 
-        stat = gcs.stat(gcs_filename)
+        stat = LazyStat(gcs_filename)
 
+        if mimetype is None:
+            mimetype = stat.content_type
+
+        resp = flask.current_app.response_class('', mimetype=mimetype)
+
+        resp.cache_control.public = True
+
+        if add_etags:
+            resp.set_etag(etags or stat.etag)
+
+        if as_attachment:
+            if attachment_filename is None:
+                attachment_filename = filename
+
+            resp.headers.add('Content-Disposition', 'attachment',
+                             filename=attachment_filename)
+
+        if add_last_modified and stat.st_ctime:
+            resp.last_modified = last_modified or int(stat.st_ctime)
+
+        resp.headers[blobstore.BLOB_KEY_HEADER] = str(blobkey.get_result())
     except gcs.NotFoundError:
         logger.warning("GCS file %r was not found", gcs_filename)
         flask.abort(404)
-
-    if mimetype is None:
-        mimetype = stat.content_type
-
-    resp = flask.current_app.response_class('', mimetype=mimetype)
-    resp.headers[blobstore.BLOB_KEY_HEADER] = str(blobkey)
-
-    resp.cache_control.public = True
-
-    if add_etags:
-        resp.set_etag(stat.etag)
-
-    if as_attachment:
-        if attachment_filename is None:
-            attachment_filename = filename
-
-        resp.headers.add('Content-Disposition', 'attachment',
-                         filename=attachment_filename)
-
-    if stat.st_ctime:
-        resp.last_modified = int(stat.st_ctime)
 
     return resp
 
--- tests/test_cloudstore.py
+++ tests/test_cloudstore.py
@@ -1,13 +1,7 @@
-import datetime
-import time
-
-import flask
 import mock
+import flask
 
 import cloudstorage as gcs
-from google.appengine.ext import blobstore
-from google.appengine.api import app_identity
-
 from flask.ext import gae
 
 
@@ -21,15 +15,16 @@ class SendGCSTestCase(gae.testing.TestCase):
                 filename,
                 bucket=flask.request.args.get('bucket', None),
                 mimetype=flask.request.args.get('mimetype', None),
-                add_etags=(not flask.request.args.has_key('noetag')),
-                as_attachment=flask.request.args.has_key('attachment'),
+                add_etags=('noetag' not in flask.request.args),
+                add_last_modified=('nolastmod' not in flask.request.args),
+                as_attachment=('attachment' in flask.request.args),
                 attachment_filename=flask.request.args.get(
                     'attachment_filename', None))
 
         return app
 
     def test_get(self):
-        f = self.create_gcs_file('test.txt', mimetype='text/plain')
+        self.create_gcs_file('test.txt', mimetype='text/plain')
 
         resp = self.client.get('/test.txt')
         self.assertBlobkey(resp, filename='test.txt')
@@ -54,15 +49,20 @@ class SendGCSTestCase(gae.testing.TestCase):
         resp1 = self.client.get('/file-missing')
         self.assert404(resp1)
 
-        resp2 = self.client.get('/file-missing?mimetype=text/plain')
-        self.assert404(resp2)
+        # TODO: Fix this. No way of asserting the file exists without calling
+        # the GCS api. Which in turn means 2 api calls. Which means slow.
+
+        # resp2 = self.client.get(
+        #    '/file-missing?mimetype=text/plain&noetag=1&nolastmod=1')
+        # self.assert404(resp2)
 
     def test_etags(self):
         self.create_gcs_file('test.txt', mimetype='text/plain')
 
         resp1 = self.client.get('/test.txt')
         # Etag of an empty file
-        self.assertEqual(resp1.get_etag()[0], 'd41d8cd98f00b204e9800998ecf8427e')
+        self.assertEqual(resp1.get_etag()[0],
+                         'd41d8cd98f00b204e9800998ecf8427e')
 
         resp2 = self.client.get('/test.txt?noetag=1')
         self.assertIsNone(resp2.get_etag()[0])
@@ -78,4 +78,10 @@ class SendGCSTestCase(gae.testing.TestCase):
             '/test.txt?attachment=1&attachment_filename=wizboingbounce')
         self.assertEqual(resp1.headers['Content-Disposition'],
                          "attachment; filename=wizboingbounce")
+
+    @mock.patch.object(gcs, 'stat', wraps=gcs.stat)
+    def test_no_stat_call(self, gcs_stat):
+        self.client.get(
+            '/file-missing?mimetype=text/plain&noetag=1&nolastmod=1')
+        self.assertFalse(gcs_stat.called)
 
